fc2cccebab87e42c2cba2ecdd856738e7026aad6
Prayash Thapa <prayasht@gmail.com>
2023-12-15 23:20:45 -0800

    Initial Commit

---
+++ PrayCrawler/PrayCrawlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayCrawlerApp.swift
+//  PrayCrawler
+//
+//  Created by Prayash Thapa on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct PrayCrawlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
